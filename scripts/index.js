b1c7c52f4d68ffdc9eab71f2b5846ba6599ae1ae
Aiza Savvinova <hmstrayza@yandex.ru>
2023-03-21 10:29:48 +0600

    for review

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+console.log("Privsdadfafet");
+
+const popupElement = document.querySelector(".popup");
+const popupCloseButtonElement = popupElement.querySelector(".popup__close");
+const popupEditButtonElement = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subtitle");
+const formProfileName = popupElement.querySelector("#name");
+const formProfileDescription = popupElement.querySelector("#description");
+const popupEditForm = popupElement.querySelector("form");
+const popupSubmit = popupElement.querySelector(".popup__submit");
+
+const openPopup = function () {
+    popupElement.classList.add("popup_opened");
+    console.log("Open popup clicked");
+};
+
+const closePopup = function () {
+    popupElement.classList.remove("popup_opened");
+};
+
+const closePopupByClickOnOverlay = function (event) {
+    if (event.target !== event.currentTarget) {
+        return;
+    }
+    closePopup();
+};
+
+const submitPopup = function (event) {
+    event.preventDefault();
+    profileName.textContent = formProfileName.value;
+    profileDescription.textContent = formProfileDescription.value;
+    closePopup();
+};
+
+popupEditButtonElement.addEventListener("click", openPopup);
+popupCloseButtonElement.addEventListener("click", closePopup);
+popupElement.addEventListener("click", closePopupByClickOnOverlay);
+popupEditForm.addEventListener("submit", submitPopup);
